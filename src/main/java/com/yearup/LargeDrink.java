b8856a44ef13fbd2fd51f4bf8e8531440f63591f
MoeSulz <msuleiman@appdev.yearup.org>
2023-06-01 14:56:27 -0400

    Successfully running program

--- src/main/java/com/yearup/LargeDrink.java
+++ src/main/java/com/yearup/LargeDrink.java
@@ -1,2 +1,14 @@
-package com.yearup;public class LargeDrink {
+package com.yearup;
+
+public class LargeDrink {
+    private double largeDrink;
+    public double getLargeDrink(){
+        return 3.00;
+    }
+
+    @Override
+    public String toString() {
+        return "LargeDrink{" + largeDrink +
+                '}';
+    }
 }
